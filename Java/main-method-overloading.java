5baf8a2f9b47cc47390689aaf36eed8712ca6f9f
Hemanth M <hemking2405@gmail.com>
2024-05-14 13:25:22 +0530

    Create main-method-overloading.java

---
+++ Java/main-method-overloading.java
@@ -0,0 +1,15 @@
+
+public class Main
+{
+	public static void main(int arg) {
+		System.out.println("Hello");
+	}
+		public static void main(String ar) {
+		System.out.println("World");
+	}
+		public static void main(String[] args) {
+		System.out.println("Hello World");
+		main(3);
+		main("Ski");
+	}
+}
