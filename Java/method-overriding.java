25585ab5c73af538d0ebf2096deee1d8a33ebf13
Hemanth M <hemking2405@gmail.com>
2024-05-14 13:26:45 +0530

    Create method-overriding.java

---
+++ Java/method-overriding.java
@@ -0,0 +1,33 @@
+class pradeep
+{
+    void nikithaa()
+    {
+        System.out.println("lovers");
+    }
+}
+class mamakuttyy
+{
+    void nikithaa()
+    {
+        System.out.println("Ex lover");
+    }
+}
+class Revii
+{
+    void nikithaa()
+    {
+        System.out.println("Besti");
+    }
+}
+public class Main
+{
+	public static void main(String[] args) {
+	    System.out.println("love today");
+	    pradeep a=new pradeep();
+	    mamakuttyy b=new mamakuttyy();
+	    Revii c=new Revii();
+	    a.nikithaa();
+	     b.nikithaa();
+	      c.nikithaa();
+	}
+}
