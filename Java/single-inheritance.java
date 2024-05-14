983843b5a988d8f0cbe49cf376461d064b364eff
Hemanth M <hemking2405@gmail.com>
2024-05-14 13:27:38 +0530

    Create single-inheritance.java

---
+++ Java/single-inheritance.java
@@ -0,0 +1,17 @@
+
+public class Main extends Terv
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		Main t = new Main();
+        System.out.println(t.ap());
+	}
+}
+
+class Terv {
+    int ap(){
+    int i=0;
+    return i;
+}
+}
