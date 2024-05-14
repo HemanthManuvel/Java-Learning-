936c9a04d3aaf0b3bb677da41b48a42a77cbf75b
Hemanth M <hemking2405@gmail.com>
2024-05-14 13:23:00 +0530

    Create java-conversion.java

---
+++ Java/java-conversion.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s=new Scanner(System.in);
+	    int n=s.nextInt();
+	    System.out.println(Integer.toBinaryString(n));
+	}
+}
+//decimal to binary:
+s.o.p(Integer.toBinaryString(input));
+//Binary to decimal:
+ String n=s.next();
+S.o.p(Integer.parseInt(n,2));
+//deimal to octal:
+S.o.p(Integer.toOctalString(n));
+//octal to decimal:
+  String n=s.next();
+S.o.p(Integer.parseInt(n,8));
+//decimal to hexadecimal:
+S.o.p(Integer.toHexString(n));
+//hexadecimal to decimal: 
+String n=s.next();
+S.o.p(Integer.parseInt(n,16));
+
+
