433cf8d328a391b6c917c433519fdb1c5236ee20
Hemanth M <hemking2405@gmail.com>
2024-05-14 13:19:53 +0530

    Create dimond.java

---
+++ Java/dimond.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s=new Scanner(System.in);
+	    int n=s.nextInt();
+	        for(int i=1;i<=n;i++)
+	        {
+	            for(int j=i;j<n;j++)
+	              {System.out.print(" ");}
+	            for(int k=1;k<=2*i-1;k++)
+	             {
+	                 System.out.print("*"); }
+	              System.out.println();
+	        }
+	        for(int i=n-1;i>=1;i--)
+	        {
+	            for(int j=i;j<n;j++)
+	              {System.out.print(" ");}
+	            for(int k=1;k<=2*i-1;k++)
+	             {System.out.print("*"); }
+	              System.out.println();
+	        }
+	}
+}
