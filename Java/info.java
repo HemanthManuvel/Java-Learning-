d524ee88e85d60ef47ae13ca2d738eeeeda98867
Hemanth M <hemking2405@gmail.com>
2024-05-14 13:24:48 +0530

    Create info.java

---
+++ Java/info.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.*;
+public class Main
+{
+	public static void main(String[] args) {
+	    Scanner s=new Scanner(System.in);
+	    
+		System.out.print("Roll no : ");
+		int r=s.nextInt();
+		System.out.print("Name : ");
+		String n=s.next();
+		System.out.print("CGPA : ");
+		int c=s.nextInt();
+		System.out.print("Dep ");
+		String d=s.next();
+		s.nextLine();
+		System.out.print("Address : ");
+		String a=s.nextLine();
+	}
+}
