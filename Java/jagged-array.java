42fc0928b491063e721a199baa52965a64c3b589
Hemanth M <hemking2405@gmail.com>
2024-05-14 13:22:35 +0530

    Create jagged-array.java

---
+++ Java/jagged-array.java
@@ -0,0 +1,11 @@
+public class Main
+{
+	public static void main(String[] args) {
+		int a[][]={new int []{1,2,3,4},new int []{1,2,3,4,5,6},  new int[]{1,2}};
+		for(int i=0;i<a.length;i++){
+		    for(int j=0;j<a[i].length;j++){
+		        System.out.print(a[i][j]+" ");
+		    } System.out.println();
+		}
+	}
+}
